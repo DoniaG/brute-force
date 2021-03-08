1a23c6482ce4da9be755484acf8864c04d27fd87
dghani <dghani@gmail.com>
2021-03-08 13:59:07 +0100

    init commit

--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -1,16 +1,39 @@
 package ma.sqli.brute.force;
 
+import java.util.HashMap;
+
 /**
  * @author : El Mahdi Benzekri
  * @since : 3/7/21, dim.
  **/
 public class BruteForceApp {
 
-    public String login(String admin, String password) {
+            private static String username;
+            private  static String mdp;
+
+
+    public static String login(String admin, String password) {
+
+        if (admin.equals("sqli") && password.equals(mdp) )
+            return"Welcome "+admin+"!";
+
+        else if (admin.equals(username) && password.equals(mdp)) {
+            return"Welcome "+admin+"!";
+
+        } else if (admin != username || password !=mdp) {
+            for (int count=0; count<=3; count++) {
+                if (count == 0)
+                {return "User or password are incorrect.";}
+
+                return "Multiple erroneous credentials, your account is locked.";
+            }
+        }
         return "";
+
     }
 
-    public void addUser(String admin, String s) {
-
+    public static void addUser(String admin, String s) {
+        HashMap users = new HashMap();
+        users.put(admin,s);
     }
 }
